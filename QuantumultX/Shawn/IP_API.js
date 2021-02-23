9b20336c84281d254274e007b15143ad04ec99f7
jenniferak <70758954+jenniferak@users.noreply.github.com>
2021-02-24 01:41:00 +0800

    Update IP_API.js

--- QuantumultX/Shawn/IP_API.js
+++ QuantumultX/Shawn/IP_API.js
@@ -37,7 +37,7 @@ if ($response.statusCode != 200) {
   var body = $response.body;
   var obj = JSON.parse(body);
   var title =flags.get(obj['countryCode']) + ' ' + Area_check(obj['country']);
-  var subtitle = ValidCheck(obj['city']) + ' - '+ ISP_ValidCheck(obj['org']);
+  var subtitle = City_ValidCheck(obj['city']) + ' - '+ ISP_ValidCheck(obj['org']);
   var ip = obj['query'];
   var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
   $done({title, subtitle, ip, description});
